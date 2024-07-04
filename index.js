78624a55cc8bc65fddce619eb73ca0cfccc7314f
Stannyut <stanley.njuguna@student.moringaschool.com>
2024-07-04 04:50:58 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+//july 2024
 const currentUser = 'Grace Hopper'
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
 const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
+
